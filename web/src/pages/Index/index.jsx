cf19a1f917d035e3640455c2548399a86131ebd1
AramosA0 <aldoramosaro@gmail.com>
2022-10-07 11:53:54 -0500

    prueba

--- web/src/pages/Index/index.jsx
+++ web/src/pages/Index/index.jsx
@@ -1,11 +1,33 @@
 import { useEffect,useState } from "react";
 
+// Material
+import {Container, Button} from "@mui/material"
+
+// Import CSS
+import "./index.css"
+
 const Index = () =>{
     console.log('hola')
     return(
-        <div>
-            <h1>Hola</h1>
-        </div>
+        <>
+            <Container fixed className="index-container">
+                <div className="index-titulo">
+                    <h1>Taller</h1>
+                </div>
+                <div className="index-titulo-descripcion">
+                    <div>
+                        <h5>Un taller para todas las persona</h5>
+                    </div>
+                    <div>
+                        <p>Control del estado de tu</p>
+                        <p>Vehiculo en Tiempo Real</p>
+                    </div>
+                    <div className="index-titulo-btn">
+                        <Button variant="outlined" className="index-titulo-btn-btn">Encuentranos</Button>
+                    </div>
+                </div>
+            </Container>
+        </>
     );
 }
 

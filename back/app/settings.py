9dbf113657ce09f371b194cb99cb828baf9d3466
Robert Charca <trevoraobac@gmail.com>
2022-10-15 11:59:44 -0500

    Aplicaciones agregadas al proyecto.

--- back/app/settings.py
+++ back/app/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'back_rest_api',
+    'back_foundation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

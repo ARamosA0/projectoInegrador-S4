cd5b523c435266786236e2832e938bfef626c2b9
Robert Charca <trevoraobac@gmail.com>
2022-10-18 21:22:55 -0500

    Implementacion del serializer de Auto y pruebas a este.

--- back/back_rest_api/serializers.py
+++ back/back_rest_api/serializers.py
@@ -5,5 +5,30 @@ from back_foundation.models import *
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Usuario_usu
-        fields = ('id', 'usu_nombres', 'usu_email', 'usu_password', 'usu_celular', 'usu_fecregistro', 'usu_fecmodificacion')
+        fields = '__all__'
         read_only_fields = ('usu_fecregistro', 'usu_fecmodificacion')
+
+class AutoSerializer(serializers.ModelSerializer):
+    marca = serializers.SlugField(
+        queryset=Marca_mar.objects.all(), read_only=True, slug_field='mar_nombre'
+    )
+
+    usuario = serializers.SlugRelatedField(
+        queryset= Usuario_usu.objects.all(), read_only=True, slug_field='usu_nombres'
+    )
+
+    class Meta:
+        model = Auto_aut
+        fields = (
+            'marca',
+            'usuario',
+            'aut_placa',
+            'aut_color',
+            'aut_imagen',
+            'aut_modelo',
+            'aut_descripcion',
+            'aut_fecadquisicion',
+            'aut_fecregistro',
+            'aut_fecmodificacion'
+        )
+        read_only_fields = ('marca', 'usuario', 'aut_fecregistro', 'aut_fecmodificacion')
--- back/back_rest_api/urls.py
+++ back/back_rest_api/urls.py
@@ -3,8 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('users/', views.UsuarioAPIGeneral.as_view()),
-    path('users/<int:usuario_id>/', views.UsuarioAPIDetallado.as_view()),
+    path('usuario/', views.UsuarioAPIGeneral.as_view()),
+    path('usuario/<int:usuario_id>/', views.UsuarioAPIDetallado.as_view()),
+    path('auto/', views.AutoAPIGeneral.as_view()),
+    path('auto/<int:auto_id>', views.AutoAPIDetallado.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

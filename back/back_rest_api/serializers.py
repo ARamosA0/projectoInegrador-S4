2df4d0857d1cd1b3a25a0aa1fe53c3099783edbb
Robert Charca <trevoraobac@gmail.com>
2022-10-20 17:42:35 -0500

    Problema con AutoSerializer solucionado

--- back/back_rest_api/serializers.py
+++ back/back_rest_api/serializers.py
@@ -9,27 +9,7 @@ class UsuarioSerializer(serializers.ModelSerializer):
         read_only_fields = ('usu_fecregistro', 'usu_fecmodificacion')
 
 class AutoSerializer(serializers.ModelSerializer):
-    """
-    marca = serializers.SlugRelatedField(
-        read_only=True, slug_field='aut_marca'
-    )
-
-    usuario = serializers.SlugRelatedField(
-        read_only=True, slug_field='aut_usuario'
-    )"""
-
     class Meta:
         model = Auto_aut
-        fields = [
-            'aut_marca',
-            'aut_usuario',
-            'aut_placa',
-            'aut_color',
-            'aut_imagen',
-            'aut_modelo',
-            'aut_descripcion',
-            'aut_fecadquisicion',
-            'aut_fecregistro',
-            'aut_fecmodificacion'
-        ]
-        read_only_fields = ['aut_marca', 'aut_usuario','aut_fecregistro', 'aut_fecmodificacion']
+        fields = '__all__'
+        read_only_fields = ['aut_fecregistro', 'aut_fecmodificacion']

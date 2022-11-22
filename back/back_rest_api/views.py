56a75c0bb3d86dbe3a8dd447c63ae00f93041ad4
AramosA0 <aldoramosaro@gmail.com>
2022-11-22 11:02:33 -0500

    a

--- back/back_rest_api/views.py
+++ back/back_rest_api/views.py
@@ -366,7 +366,7 @@ class RegistroErroresManuales(APIView):
 
 class RegistroErroresManualesDetalle(APIView):
     def get(self, request, autoid):
-        errmanual = RegistroManual_rma.get(auto=autoid)
+        errmanual = RegistroManual_rma.objects.get(auto=autoid)
         serializer = ErrManualSerializer(errmanual, many=True)
 
         return Response(serializer.data)

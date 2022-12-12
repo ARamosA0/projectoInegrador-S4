2f9cb7cf66c62a67a83f5efb3d5032e5d7652a9a
AramosA0 <aldoramosaro@gmail.com>
2022-12-12 12:23:27 -0500

    db

--- back/app/settings.py
+++ back/app/settings.py
@@ -105,28 +105,28 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # }
 
 
-#BD DIGITAL OCEAN
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'defaultdb',
-#         'USER': 'doadmin',
-#         'PASSWORD': 'AVNS_N1KB4on2mcZGbRDgSlu',
-#         'HOST': 'db-mysql-nyc1-55843-do-user-12058349-0.b.db.ondigitalocean.com',
-#         'PORT': '25060',
-#      }
-# }
-
+BD DIGITAL OCEAN
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'Y3rJ9aszMyicbaF2zqda',
-        'HOST': 'containers-us-west-170.railway.app',
-        'PORT': '5752',
+        'NAME': 'defaultdb',
+        'USER': 'doadmin',
+        'PASSWORD': 'AVNS_N1KB4on2mcZGbRDgSlu',
+        'HOST': 'db-mysql-nyc1-55843-do-user-12058349-0.b.db.ondigitalocean.com',
+        'PORT': '25060',
      }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'railway',
+#         'USER': 'root',
+#         'PASSWORD': 'Y3rJ9aszMyicbaF2zqda',
+#         'HOST': 'containers-us-west-170.railway.app',
+#         'PORT': '5752',
+#      }
+# }
 
 
 
